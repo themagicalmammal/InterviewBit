6f48e2d09235eb3c5aca0fbf365dc7e9de1a569c
Dipan <dipangithub@gmail.com>
2021-03-30 20:20:02 +0530

    NESTED_CMPL2

---
+++ Level 1 - Time Complexity/NESTED_CMPL2.py
@@ -0,0 +1,25 @@
+'''
+What is the time complexity of the following code :
+
+    int a = 0;
+    for (i = 0; i < N; i++) {
+        for (j = N; j > i; j--) {
+            a = a + i + j;
+        }
+    }
+
+ O(N)
+ O(N*log(N))
+ O(N * Sqrt(N))
+ O(N*N)
+'''
+
+O(N*N)
+
+Total number of runs = N + (N - 1) + (N - 2) + ... 1 + 0
+
+= N * (N + 1) / 2
+
+= 1/2 * N^2 + 1/2 * N
+
+O(N^2) times.
