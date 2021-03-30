138394ecbf33a57672d50f5a82e49b7bf3b3bee5
Dipan <dipangithub@gmail.com>
2021-03-30 20:29:42 +0530

    WHILE_CMPL

---
+++ Level 1 - Time Complexity/WHILE_CMPL.py
@@ -0,0 +1,21 @@
+'''
+What is the time complexity of the following code :
+
+        int a = 0, i = N;
+        while (i > 0) {
+            a += i;
+            i /= 2;
+        }
+
+ O(N)
+ O(Sqrt(N))
+ O(N / 2)
+ O(log N)
+ O(log(log N))
+'''
+
+O(log N)
+
+We have to find the smallest x such that `N / 2^x < 1 OR 2^x > N`
+
+x = log(N) 
