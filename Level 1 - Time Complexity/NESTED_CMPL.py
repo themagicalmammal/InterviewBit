b27e9e9aeb1caebb68ca71481e13542b01a63669
Dipan <dipangithub@gmail.com>
2021-03-30 20:16:51 +0530

    NESTED_CMPL

---
+++ Level 1 - Time Complexity/NESTED_CMPL.py
@@ -0,0 +1,26 @@
+'''
+What is the time, space complexity of following code :
+
+    int a = 0, b = 0;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            a = a + j;
+        }
+    }
+    for (k = 0; k < N; k++) {
+        b = b + k;
+    }
+
+ O(N * N) time, O(1) space
+ O(N) time, O(N) space
+ O(N) time, O(N) space
+ O(N * N) time, O(N) space
+ O(N * N * N) time, O(1) space
+'''
+
+O(N * N) time, O(1) space
+
+The first set of nested loops is O(N^2) and the second loop is O(N).
+
+This is O(max(N^2,N)) which is O(N^2).
+
