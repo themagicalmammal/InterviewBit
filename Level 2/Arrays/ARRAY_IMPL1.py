54fa6ba898c01939d59279d633bd73377df8871b
Dipan <dipangithub@gmail.com>
2021-03-31 14:14:56 +0530

    ARRAY_IMPL1

---
+++ Level 2/Arrays/ARRAY_IMPL1.py
@@ -0,0 +1,19 @@
+'''
+
+def performOps(A):
+    blen = 2 * len(A)
+    B = [0]*blen
+    for i in xrange(len(A)):
+        B[i] = A[i]
+        B[i + len(A)] = A[(len(A) - i) % len(A)]
+    return B
+
+Lets say performOps was called with A : [5, 10, 2, 1]. What would be the output of the following call :
+
+B = performOps(A)
+for i in xrange(len(B)):
+    print B[i],
+
+'''
+
+5 10 2 1 5 1 2 10
