68c772266e6173a77c64c8748e00c30410124fd8
Dipan <dipangithub@gmail.com>
2021-04-06 01:30:21 +0530

    Spiral Order Matrix II

--- Level 2/Arrays/Spiral Order Matrix I.py
+++ Level 2/Arrays/Spiral Order Matrix I.py
@@ -29,22 +29,22 @@ class Solution:
      r = cols-1
      dir = 0
      while t<=b and l<=r:
-        if(dir ==0):
+        if dir ==0:
             for i in range(l,r+1):
                 ret.append(a[t][i])
             t+=1
             dir =1
-        elif(dir ==1):
+        elif dir ==1:
             for i in range(t, b+1):
                 ret.append(a[i][r])
             r-=1
             dir =2
-        elif(dir ==2):
+        elif dir ==2:
             for i in range(r,l-1,-1):
                 ret.append(a[b][i])
             b-=1
             dir = 3
-        elif(dir ==3):
+        elif dir ==3:
             for i in range(b, t-1, -1):
                 ret.append(a[i][l])
             l+=1
