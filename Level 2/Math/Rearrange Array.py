28999734faf064f466d289189ff2bbcd6f2f4513
Dipan <dipangithub@gmail.com>
2021-04-09 16:39:11 +0530

    Rearrange Array

---
+++ Level 2/Math/Rearrange Array.py
@@ -0,0 +1,19 @@
+'''
+
+Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
+
+Example:
+
+Input : [1, 0]
+Return : [0, 1]
+
+'''
+
+class Solution:
+    def arrange(self, A):
+        n = len(A)
+        for i in range(n):
+            A[i] += (A[A[i]]%n)*n
+        for i in range(n):
+            A[i] //= n
+        return A
