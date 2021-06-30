c1c2c865cb3828207fa065d6ca92626d650b6b08
Dipan Nanda <dipangithub@gmail.com>
2021-06-30 23:09:04 +0530

    Largest Coprime Divisor

---
+++ Level 2/Math/Largest Coprime Divisor.py
@@ -0,0 +1,22 @@
+'''
+
+You are given two positive numbers A and B. You need to find the maximum valued integer X such that:
+
+    X divides A i.e. A % X = 0
+    X and B are co-prime i.e. gcd(X, B) = 1
+
+For example,
+
+A = 30
+B = 12
+We return
+X = 5
+
+'''
+
+from math import gcd
+class Solution:
+    def cpFact(self, A, B):
+        while gcd(A,B)!=1:
+            A//=gcd(A,B)
+        return A
